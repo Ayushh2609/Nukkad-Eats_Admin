5234bc54eb191f728d4d5ba47bcf3945a9e9ec74
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-02 20:03:37 +0530

    Login Screen SignUp Screen

--- app/src/main/java/com/example/nukkadeatsadmin/SignupActivity.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/SignupActivity.kt
@@ -1,20 +1,37 @@
 package com.example.nukkadeatsadmin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nukkadeatsadmin.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private val binding : ActivitySignupBinding by lazy {
+        ActivitySignupBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.alreadyHaveAccount.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.signUpCreateButton.setOnClickListener{
+            val intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
