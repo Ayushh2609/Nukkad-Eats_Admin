0682a0c6c1fc1bf89ff98366d2563b2c3bf63d51
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-06 03:05:24 +0530

    Some changes

--- app/src/main/java/com/example/nukkadeatsadmin/AdminProfileActivity.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/AdminProfileActivity.kt
@@ -1,7 +1,10 @@
 package com.example.nukkadeatsadmin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -23,6 +26,47 @@ class AdminProfileActivity : AppCompatActivity() {
 
         binding.backButton.setOnClickListener {
             finish()
+        }
+
+        binding.nameEditText.isEnabled = false
+        binding.addressEditText.isEnabled = false
+        binding.emailEditText.isEnabled = false
+        binding.phoneEditText.isEnabled = false
+        binding.passwordEditText.isEnabled = false
+        binding.imgRes.isEnabled = false
+
+        var isEnable = false
+        binding.editButton.setOnClickListener {
+            isEnable = !isEnable
+
+            binding.nameEditText.isEnabled = isEnable
+            binding.addressEditText.isEnabled = isEnable
+            binding.emailEditText.isEnabled = isEnable
+            binding.phoneEditText.isEnabled = isEnable
+            binding.passwordEditText.isEnabled = isEnable
+            binding.imgRes.isEnabled = isEnable
+
+
+            if(isEnable){
+                binding.nameEditText.requestFocus()
+
+                binding.imgRes.setOnClickListener{
+                    pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                }
+            }
+        }
+
+        binding.saveInfoBtn.setOnClickListener {
+            Toast.makeText(this, "Information Saved" , Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    var imageSelected = false
+    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            binding.imgRes.setImageURI(uri)
+            imageSelected = true
         }
     }
 }
