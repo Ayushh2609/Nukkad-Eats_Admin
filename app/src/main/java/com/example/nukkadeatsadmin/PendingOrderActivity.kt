f675f6b73fd601dd2e047d9a857172f542d0afa4
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-06 03:06:17 +0530

    Some changes

---
+++ app/src/main/java/com/example/nukkadeatsadmin/PendingOrderActivity.kt
@@ -0,0 +1,36 @@
+package com.example.nukkadeatsadmin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nukkadeatsadmin.adapters.PendingItemAdapter
+import com.example.nukkadeatsadmin.databinding.ActivityPendingOrderBinding
+
+class PendingOrderActivity : AppCompatActivity() {
+    private val binding : ActivityPendingOrderBinding by lazy{
+        ActivityPendingOrderBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val customerList = arrayListOf("Ayush" , "Anchal" , "Abhijeet" , "Munni")
+        val quantityList = arrayListOf("3" , "4" , "1" , "9")
+
+
+        val adapter = PendingItemAdapter(customerList , quantityList)
+        binding.pendingRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.pendingRecyclerView.adapter = adapter
+        
+    }
+}
