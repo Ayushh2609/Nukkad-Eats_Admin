04e445ebb0d16e96330c1eb150b0bee9a4a696c8
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-06 02:04:07 +0530

    Feedback xml start

--- app/src/main/java/com/example/nukkadeatsadmin/adapters/AllItemAdapter.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/adapters/AllItemAdapter.kt
@@ -3,13 +3,21 @@ package com.example.nukkadeatsadmin.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.nukkadeatsadmin.AllItemMenuActivity
 import com.example.nukkadeatsadmin.databinding.CartItemsBinding
 
-class AllItemAdapter(private val foodName : MutableList<String> , private val foodPrice : MutableList<String> , private val foodDescription : MutableList<String>) : RecyclerView.Adapter<AllItemAdapter.viewHolder>() {
+class AllItemAdapter(
+    private val foodNameList: ArrayList<String>,
+    private val foodPriceList: ArrayList<String>,
+    private val foodImageList: ArrayList<Int>
+) : RecyclerView.Adapter<AllItemAdapter.viewHolder>() {
+
+
+    private val itemQuantity = IntArray(foodNameList.size) { 1 }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllItemAdapter.viewHolder {
-        val binding = CartItemsBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
+
+
+        val binding = CartItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return viewHolder(binding)
     }
 
@@ -18,12 +26,60 @@ class AllItemAdapter(private val foodName : MutableList<String> , private val fo
         holder.bind(position)
     }
 
-    override fun getItemCount(): Int = foodName.size
+    override fun getItemCount(): Int = foodNameList.size
 
-    inner class viewHolder(private val binding : CartItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class viewHolder(private val binding: CartItemsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
-            binding.apply {  }
+            binding.apply {
+                foodName.text = foodNameList[position]
+                foodPrice.text = foodPriceList[position]
+                foodImage.setImageResource(foodImageList[position])
+
+
+                plusCart.setOnClickListener {
+                    increaseItem(position)
+                }
+
+                minusCart.setOnClickListener {
+                    decreaseItem(position)
+                }
+
+                deleteCart.setOnClickListener {
+                    deleteItem(position)
+                }
+            }
+        }
+
+
+        private fun decreaseItem(position: Int) {
+            if (itemQuantity[position] > 1) {
+                itemQuantity[position]--
+                binding.quantityCart.text = itemQuantity[position].toString()
+            }
+            if (itemQuantity[position] == 1) {
+                deleteItem(position)
+            }
+        }
+
+        private fun deleteItem(position: Int) {
+            foodNameList.removeAt(position)
+            foodPriceList.removeAt(position)
+            foodImageList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, foodNameList.size)
+
+        }
+
+        private fun increaseItem(position: Int) {
+
+            if (itemQuantity[position] < 60) {
+                itemQuantity[position]++
+                binding.quantityCart.text = itemQuantity[position].toString()
+
+            }
         }
 
     }
+
 }
