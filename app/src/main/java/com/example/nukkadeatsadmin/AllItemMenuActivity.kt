1b99287124f16a40123af46264aed2e3faec095d
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-06 02:05:06 +0530

    Some changes

--- app/src/main/java/com/example/nukkadeatsadmin/AllItemMenuActivity.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/AllItemMenuActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nukkadeatsadmin.adapters.AllItemAdapter
+import com.example.nukkadeatsadmin.databinding.ActivityAllItemMenuBinding
 
 class AllItemMenuActivity : AppCompatActivity() {
+    private val binding : ActivityAllItemMenuBinding by lazy {
+        ActivityAllItemMenuBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_all_item_menu)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+        val foodNameList = arrayListOf("Burger" , "Spring roll" , "Parathe" , "Sydney Sweeney kachi Ghani")
+        val foodPriceList = arrayListOf("50" , "40" , "30" , "6969")
+        val foodImageList = arrayListOf(R.drawable.burger , R.drawable.pizza , R.drawable.burger , R.drawable.sydney)
+
+        val adapter = AllItemAdapter(foodNameList , foodPriceList , foodImageList)
+
+        binding.recyclerItems.layoutManager = LinearLayoutManager(this)
+        binding.recyclerItems.adapter = adapter
     }
 }
