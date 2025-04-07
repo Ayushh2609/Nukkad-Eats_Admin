16414869afbf2785bd6062bf880692449f8e6706
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-07 22:38:23 +0530

    Firebase Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -47,12 +48,23 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
-    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation("com.google.firebase:firebase-auth-ktx:23.2.0")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    apply(plugin = "com.google.gms.google-services")
+
+
 }
---
+++ app/src/main/java/com/example/nukkadeatsadmin/Modal/UserModal.kt
@@ -0,0 +1,8 @@
+package com.example.nukkadeatsadmin.Modal
+
+data class UserModal(
+    val name : String? = null,
+    val resName : String? = null,
+    val email : String? = null,
+    val password : String? = null
+)
