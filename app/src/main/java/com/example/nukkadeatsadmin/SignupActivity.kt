a41ea49651d2eaf0b3789fb919a7521d3a76c99f
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-07 12:29:46 +0530

    Backend Start

--- app/src/main/java/com/example/nukkadeatsadmin/SignupActivity.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/SignupActivity.kt
@@ -2,14 +2,29 @@ package com.example.nukkadeatsadmin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nukkadeatsadmin.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var username : String
+    private lateinit var restaurantName : String
+    private lateinit var email : String
+    private lateinit var password : String
+    private lateinit var database : DatabaseReference
 
     private val binding : ActivitySignupBinding by lazy {
         ActivitySignupBinding.inflate(layoutInflater)
@@ -24,14 +39,34 @@ class SignupActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //Initialize Firebase Database
+        database = Firebase.database.reference
+
+
 
         binding.alreadyHaveAccount.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
         }
 
         binding.signUpCreateButton.setOnClickListener{
-            val intent = Intent(this , MainActivity::class.java)
-            startActivity(intent)
+
+            //Getting text from edit texts(User) and saving it to the variables
+            username = binding.editTextUserName.text.toString().trim()
+            restaurantName = binding.editTextRestroName.text.toString().trim()
+            email = binding.editTextTextEmailAddress.text.toString().trim()
+            password = binding.editTextTextPassword.text.toString().trim()
+
+            if(username.isBlank() || restaurantName.isBlank() || email.isBlank() || password.isBlank()){
+                binding.editTextUserName.error = "Enter your username"
+                binding.editTextRestroName.error = "Enter your Restaurant Name"
+                binding.editTextTextEmailAddress.error = "Enter your Email"
+                binding.editTextTextPassword.error = "Enter your Password"
+            }else{
+                createAccount(email , password)
+            }
         }
 
 
@@ -46,5 +81,19 @@ class SignupActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+                Toast.makeText(this , "Account Created Successfully", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this , LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this , "Account Creation Failed" , Toast.LENGTH_SHORT).show()
+                Log.d("Account", "createAccount: Failure",task.exception)
+            }
+        }
     }
 }
