3c0ec345b5e8157456bde4f3cedbb4349f625eb2
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-11 02:26:20 +0530

    Firebase Storage in AddItem Activity

--- app/src/main/java/com/example/nukkadeatsadmin/AddMenuActivity.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/AddMenuActivity.kt
@@ -2,9 +2,6 @@ package com.example.nukkadeatsadmin
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.PickVisualMediaRequest
@@ -13,23 +10,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isEmpty
+import com.example.nukkadeatsadmin.Modal.AllMenu
 import com.example.nukkadeatsadmin.databinding.ActivityAddMenuBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class AddMenuActivity : AppCompatActivity() {
 
-    private lateinit var foodName : String
-    private lateinit var foodPrice : String
-    private var foodImageUri : Uri? = null
-    private lateinit var foodDescription : String
-    private lateinit var foodIngredients : String
+    private lateinit var foodName: String
+    private lateinit var foodPrice: String
+    private var foodImageUri: Uri? = null
+    private lateinit var foodDescription: String
+    private lateinit var foodIngredients: String
 
     //Firebase
-    private lateinit var auth : FirebaseAuth
-    private lateinit var database : FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
 
     private val binding: ActivityAddMenuBinding by lazy {
         ActivityAddMenuBinding.inflate(layoutInflater)
@@ -52,36 +49,14 @@ class AddMenuActivity : AppCompatActivity() {
         database = FirebaseDatabase.getInstance()
 
         binding.AddItemBtn.setOnClickListener {
-
             foodName = binding.itemName.text.toString().trim()
             foodPrice = binding.itemPrice.text.toString().trim()
             foodDescription = binding.itemDescription.text.toString().trim()
             foodName = binding.itemIngredients.text.toString().trim()
-
-            if(foodName.isEmpty()){
-                binding.itemName.error = "Please enter the Name of the food"
-            }
-            if(foodPrice.isEmpty()){
-                binding.itemPrice.error = "Please enter the price of the food"
-            }
-            if(foodDescription.isEmpty()){
-                binding.itemDescription.error = "Please enter the description of the food"
-            }
-            if(foodIngredients.isEmpty()){
-                binding.itemIngredients.error = "Please enter the Ingredients of the food"
-            }
-            else {
-                Toast.makeText(this , "Item added successfully" , Toast.LENGTH_SHORT).show()
-                uploadData()
-            }
         }
 
         binding.backButton.setOnClickListener {
             finish()
-        }
-
-        binding.addImage.setOnClickListener {
-            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         binding.AddItemBtn.setOnClickListener {
@@ -96,9 +71,9 @@ class AddMenuActivity : AppCompatActivity() {
                 isValid = false
             }
             if (!imageSelected) {
-                binding.addImage.background = ContextCompat.getDrawable(this , R.drawable.red_border)
+                binding.addImage.background = ContextCompat.getDrawable(this, R.drawable.red_border)
                 isValid = false
-            }else{
+            } else {
                 binding.addImage.setBackgroundResource(0)
             }
             if (binding.itemDescription.text.toString().isEmpty()) {
@@ -111,20 +86,72 @@ class AddMenuActivity : AppCompatActivity() {
             }
             if (isValid) {
                 Toast.makeText(this, "The item has added", Toast.LENGTH_SHORT).show()
+                uploadData()
                 finish()
+            }
+
+            binding.addImage.setOnClickListener {
+                pickImage.launch("image/*")
             }
 
         }
     }
 
     private fun uploadData() {
+        //Get a reference to the "menu" node in the Database
+        val menuRef = database.getReference("menu")
+
+        // Generate a unique key for the menu Item
+        val newItemKey = menuRef.push().key
+
+
+        if (foodImageUri != null) {
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("menu_images/${newItemKey}.jpg")
+
+            val uploadTask = imageRef.putFile(foodImageUri!!)
+
+            uploadTask.addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { downloadUrl ->
+
+                    //Create a new Menu Item
+                    val newItem = AllMenu(
+                        foodName = foodName,
+                        foodPrice = foodPrice,
+                        foodDescription = foodDescription,
+                        foodIngredient = foodIngredients,
+                        foodImage = downloadUrl.toString()
+                    )
+                    newItemKey?.let { key ->
+                        menuRef.child(key).setValue(newItem).addOnSuccessListener {
+                            Toast.makeText(this, "Data Upload Successfully", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Data Upload Failed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    }
+                }
+
+            }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Image Upload Failed", Toast.LENGTH_SHORT).show()
+
+                }
+
+        } else {
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 
     var imageSelected = false
-    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         if (uri != null) {
             binding.selectedImage.setImageURI(uri)
+            foodImageUri = uri
             imageSelected = true
         }
     }
--- app/src/main/java/com/example/nukkadeatsadmin/Modal/AllMenu.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/Modal/AllMenu.kt
@@ -4,7 +4,7 @@ data class AllMenu(
 
     val foodName : String? = null,
     val foodPrice : String? = null,
-    val foodImage : Int? = null,
+    val foodImage : String? = null,
     val foodDescription : String? = null,
     val foodIngredient : String? = null
 )
