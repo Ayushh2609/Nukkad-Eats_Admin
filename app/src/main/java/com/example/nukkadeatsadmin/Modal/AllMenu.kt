ec05296b2b9e089723d5169579b9a888626c74f7
Ayushh0708 <paliwalaayush717@gmail.com>
2025-04-17 01:45:57 +0530

    Some changes

--- app/src/main/java/com/example/nukkadeatsadmin/Modal/AllMenu.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/Modal/AllMenu.kt
@@ -2,9 +2,9 @@ package com.example.nukkadeatsadmin.Modal
 
 data class AllMenu(
 
-    val foodName : String? = null,
-    val foodPrice : String? = null,
-    val foodImage : String? = null,
-    val foodDescription : String? = null,
-    val foodIngredient : String? = null
+    val foodName: String? = null,
+    val foodPrice: String? = null,
+    val foodImage: String? = null,
+    val foodDescription: String? = null,
+    val foodIngredient: String? = null
 )
