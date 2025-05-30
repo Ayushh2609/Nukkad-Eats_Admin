b0fd85fcc19df54f23b203fd050a89214cfe3b76
Ayushh0708 <paliwalaayush717@gmail.com>
2025-05-31 00:18:10 +0530

    Out for Delivery Activity Complete

--- app/src/main/java/com/example/nukkadeatsadmin/Modal/UserModal.kt
+++ app/src/main/java/com/example/nukkadeatsadmin/Modal/UserModal.kt
@@ -2,8 +2,10 @@ package com.example.nukkadeatsadmin.Modal
 
 data class UserModal(
     val name : String? = null,
+    val address : String? = null,
     val resName : String? = null,
     val email : String? = null,
+    val phone : String? = null,
     val password : String? = null,
     val loginMethod : String? = null
 )
